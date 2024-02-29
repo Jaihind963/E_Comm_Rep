827f6ad6cf3e583fa0d000ae41d93a710166b6e3
Jaihind963 <sabavatjaihind@gmail.com>
2024-02-29 21:14:42 +0530

    API url added

---
+++ lib/pages/myhomepage.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ 
+  @override
+  Widget build(BuildContext context) {
+   
+    return Scaffold(
+      appBar: AppBar(
+        
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        
+        title: Text(widget.title),
+      ),
+      body: Center(
+       
+        child: Column(
+         
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              'Hello',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), 
+    );
+  }
+  void userData()async{
+    const url = 'https://randomuser.me/api/?results=5000';
+    final uri = Uri.parse(url);
+  final response= await http.get(uri);
+  final body = response.body;
+   final json=  jsonDecode(body);
+  }
+}
